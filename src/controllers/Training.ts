e65b36e5560611c0fb59f28ab056f499050af19f
Leonardo Lobato <leonardollobato@Leonardos-MBP.home>
2019-07-03 03:35:07 +0100

    added training controller

--- src/controllers/Training.ts
+++ src/controllers/Training.ts
@@ -1,59 +1,67 @@
 import { NextFunction, Request, Response } from 'express'
-import { User } from '../models'
+import { Training } from '../models'
 
-let users: Array<User> = []
+let trainings: Array<Training> = []
 
-export let getUsers = (req: Request, res: Response, next: NextFunction) => {
-  return res.status(200).send(users)
+export let getTrainings = (req: Request, res: Response, next: NextFunction) => {
+  return res.status(200).send(trainings)
 }
 
-export let getUser = (req: Request, res: Response, next: NextFunction) => {
+export let getTraining = (req: Request, res: Response, next: NextFunction) => {
   const id = req.params.id
-  const user = users.find(objec => objec.id === id)
-  const httpStatusCode = user ? 200 : 404
-  return res.status(httpStatusCode).send(user)
+  const training = trainings.find(objec => objec.id === id)
+  const httpStatusCode = training ? 200 : 404
+  return res.status(httpStatusCode).send(training)
 }
 
-export let addUser = (req: Request, res: Response, next: NextFunction) => {
-  const user: User = {
+export let addTraining = (req: Request, res: Response, next: NextFunction) => {
+  const training: Training = {
     id: 1,
-    username: req.body.username,
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    email: req.body.email,
-    password: req.body.password,
+    name: req.body.name,
+    description: req.body.description,
+    embedURL: req.body.embedURL,
+    downloadURL: req.body.downloadURL,
+    thumbnailURL: req.body.thumbnailURL,
   }
-  users.push(user)
-  return res.status(201).send(user)
+  trainings.push(training)
+  return res.status(201).send(training)
 }
 
-export let updateUser = (req: Request, res: Response, next: NextFunction) => {
+export let updateTraining = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const id = req.params.id
-  const userIndex = users.findIndex(item => item.id === id)
+  const trainingIndex = trainings.findIndex(item => item.id === id)
 
-  if (userIndex === -1) {
+  if (trainingIndex === -1) {
     return res.status(404).send()
   }
 
-  const user = users[userIndex]
-  user.username = req.body.username || user.username
-  user.firstname = req.body.firstname || user.firstname
-  user.lastname = req.body.lastname || user.lastname
-  user.email = req.body.email || user.email
-  user.password = req.body.password || user.password
+  const training = trainings[trainingIndex]
+  training.name = req.body.name || training.name
+  training.description = req.body.description || training.description
+  training.embedURL = req.body.embedURL || training.embedURL
+  training.downloadURL = req.body.downloadURL || training.downloadURL
+  training.thumbnailURL = req.body.thumbnailURL || training.thumbnailURL
 
-  users[userIndex] = user
+  trainings[trainingIndex] = training
   return res.status(204).send()
 }
 
-export let removeUser = (req: Request, res: Response, next: NextFunction) => {
+export let removeTraining = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const id = req.params.id
-  const userIndex = users.findIndex(item => item.id === id)
+  const trainingIndex = trainings.findIndex(item => item.id === id)
 
-  if (userIndex === -1) {
+  if (trainingIndex === -1) {
     return res.status(404).send()
   }
 
-  users = users.filter(item => item.id !== id)
+  trainings = trainings.filter(item => item.id !== id)
   return res.status(204).send()
 }
